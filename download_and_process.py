9aa874a1f299538334bcbb6291248b98903036d3
Tony Chen <tchen@archedata.io>
2025-06-17 09:39:14 +0800

    chore: add logging for video download command

--- download_and_process.py
+++ download_and_process.py
@@ -52,6 +52,7 @@ def download(video_path, ytb_id, proxy=None):
         "--external-downloader-args", '"-x 16 -k 1M"'
     ])
     
+    logging.info(f"down_video command: {down_video}")
     success, output = run_command(down_video, f"Downloading video {ytb_id}")
     
     if not success:
